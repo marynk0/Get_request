bae880f227d5bac0e533955320e0ed9b8d6baf39
mary nyambura <maryj.kamunya2@gmail.com>
2023-02-17 22:30:27 +0300

    first commit

---
+++ survey.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/mj/Downloads/archive/it_ salary_survey_eu_ 2019.csv')
+# you edit the (df._) in order to run different functions
+# eg df.tail(), df.corr()
+print(df.head()) 
